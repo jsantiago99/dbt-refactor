3bc57f6e898ffef6de0e6be17a7f8eef758501f4
throwawayboi628@gmail.com <throwawayboi628@gmail.com>
2022-09-30 19:27:20 +0000

    refactored

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,91 +1,105 @@
-WITH 
+with
 
---Import CTEs
+-- Import CTEs
 
 customers as (
 
-    select * from {{ source('jaffle_shop', 'customers') }}
+  select * from {{ ref('stg_jaffle_shop__customers') }}
+
 ),
 
 orders as (
 
-    select * from {{ source('jaffle_shop', 'orders') }}
+  select * from {{ ref('stg_jaffle_shop__orders') }}
+
 ),
 
 payments as (
 
-    select * from {{ source('stripe', 'payment') }}
+  select * from {{ ref('stg_stripe__payments') }}
+
 ),
 
+-- Logical CTEs
+
+completed_payments as (
+
+  select 
+    order_id,
+    max(payment_created_at) as payment_finalized_date,
+    sum(payment_amount) as total_amount_paid
+  from payments
+  where payment_status <> 'fail'
+  group by 1
+
+),
 
 paid_orders as (
-    
-select 
 
-    Orders.ID as order_id,
-    Orders.USER_ID	as customer_id,
-    Orders.ORDER_DATE AS order_placed_at,
-    Orders.STATUS AS order_status,
-    p.total_amount_paid,
-    p.payment_finalized_date,
-    C.FIRST_NAME    as customer_first_name,
-    C.LAST_NAME as customer_last_name
-    
-FROM orders as Orders
+  select 
+    orders.order_id,
+    orders.customer_id,
+    orders.order_placed_at,
+    orders.order_status,
 
-left join (
-    
-    select 
-        
-        ORDERID as order_id, 
-        max(CREATED) as payment_finalized_date, 
-        sum(AMOUNT) / 100.0 as total_amount_paid
+    completed_payments.total_amount_paid,
+    completed_payments.payment_finalized_date,
 
-    from payments
-    where STATUS <> 'fail'
-    group by 1) p 
-    ON orders.ID = p.order_id
+    customers.customer_first_name,
+    customers.customer_last_name
+  from orders
+  left join completed_payments on orders.order_id = completed_payments.order_id
+  left join customers on orders.customer_id = customers.customer_id
 
-left join customers C on orders.USER_ID = C.ID ),
+),
 
-customer_orders as (
-    
-select 
+-- Final CTE
 
-    C.ID as customer_id, 
-    min(ORDER_DATE) as first_order_date, 
-    max(ORDER_DATE) as most_recent_order_date, 
-    count(ORDERS.ID) AS number_of_orders
+final as (
 
-from customers C 
-left join orders as Orders
-on orders.USER_ID = C.ID 
-group by 1)
+  select
+    order_id,
+    customer_id,
+    order_placed_at,
+    order_status,
+    total_amount_paid,
+    payment_finalized_date,
+    customer_first_name,
+    customer_last_name,
 
-select
+    -- sales transaction sequence
+    row_number() over (order by order_id) as transaction_seq,
 
-    p.*,
-    ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
-    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
-    CASE WHEN c.first_order_date = p.order_placed_at
-    THEN 'new'ELSE 'return' END as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos 
+    -- customer sales sequence
+    row_number() over (partition by customer_id order by order_id) as customer_sales_seq,
 
-FROM paid_orders p
-left join customer_orders as c USING (customer_id)
-LEFT OUTER JOIN 
-(
-    
-select
+    -- new vs returning customer
+    case  
+      when (
+      rank() over (
+      partition by customer_id
+      order by order_placed_at, order_id
+      ) = 1
+    ) then 'new'
+    else 'return' end as nvsr,
 
-    p.order_id,
-    sum(t2.total_amount_paid) as clv_bad
+    -- customer lifetime value
+    sum(total_amount_paid) over (
+      partition by customer_id
+      order by order_placed_at
+      ) as customer_lifetime_value,
 
-from paid_orders p
+    -- first day of sale
+    first_value(order_placed_at) over (
+      partition by customer_id
+      order by order_placed_at
+      ) as fdos
 
-    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-    group by 1
-    order by p.order_id
-) x on x.order_id = p.order_id
-ORDER BY order_id
+    from paid_orders
+		
+)
+
+-- Simple Select Statement
+
+select * from final
+order by order_id
